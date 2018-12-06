385932441cbd6c57ba36d92c1b03aae3b8d02eec
AlirezaHakamian <alireza.hakamian@gmail.com>
2018-12-06 18:16:12 +0100

    class renamed

--- MyImmutableStudent.java
+++ MyImmutableStudent.java
@@ -5,8 +5,8 @@
  * @author (your name)
  * @version (a version number or a date)
  */
-public class ImmutableStudent
+public class MyImmutableStudent
 {
-   public ImmutableStudent(){
+   public MyImmutableStudent(){
     }
 }
