b6893b5f72a744bfa14831f86ac75f2424529514
mfrank <markus.frank@informatik.tu-chemnitz.de>
2019-11-28 14:24:47 +0100

    commited initial version of exercise sheet 8 for eclipse

---
+++ src/de/unistuttgart/iste/rss/oo/hamstersimulator/exercise8/homework/MyImmutableStudent.java
@@ -0,0 +1,13 @@
+package de.unistuttgart.iste.rss.oo.hamstersimulator.exercise8.homework;
+
+/**
+ * Write a description of class ImmutableStudent here. This class must be
+ * Immutable
+ * 
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MyImmutableStudent {
+	public MyImmutableStudent() {
+	}
+}
